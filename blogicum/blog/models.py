d6c15c3c82c4a2e0a0a307fb2333a5046664acc2
Гилязова Альбина <giliazova@mail.ru>
2024-03-15 12:59:35 +0300

    Added help texts

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -13,8 +13,13 @@ class BaseModel(models.Model):
     created_at - дата создания записи в БД.
     """
 
-    is_published = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано',
+        help_text='Снимите галочку, чтобы скрыть публикацию.',
+    )
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name='Добавлено')
 
     class Meta:
         abstract = True
@@ -28,9 +33,18 @@ class Category(BaseModel):
     slug - слаг категории.
     """
 
-    title = models.CharField(max_length=256)
-    description = models.TextField()
-    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    description = models.TextField(verbose_name='Описание')
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Идентификатор',
+        help_text=('Идентификатор страницы для URL; разрешены символы '
+                   'латиницы, цифры, дефис и подчёркивание.'),
+    )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
 
 
 class Location(BaseModel):
@@ -40,7 +54,11 @@ class Location(BaseModel):
     name - название места.
     """
 
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, verbose_name='Название места')
+
+    class Meta:
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
 
 
 class Post(BaseModel):
@@ -56,11 +74,16 @@ class Post(BaseModel):
     """
 
     title = models.CharField(max_length=256)
-    text = models.TextField()
-    pub_date = models.DateTimeField()
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
+        help_text=('Если установить дату и время в будущем - можно '
+                   'делать отложенные публикации.'),
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        verbose_name='Автор публикации',
     )
     location = models.ForeignKey(
         Location,
@@ -68,10 +91,16 @@ class Post(BaseModel):
         blank=True,
         null=True,
         related_name='posts_for_location',
+        verbose_name='Местоположение',
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         null=True,
         related_name='posts_for_category',
+        verbose_name='Категория',
     )
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
